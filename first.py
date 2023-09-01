2c342635b44b5490bd2ededa78505762bcba3cdd
Arpitasenapati <108747344+Arpitasenapati@users.noreply.github.com>
2023-09-01 20:39:41 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,13 @@
+a=int(input('enter a number a'))
+b=int(input('enter a number b'))
+#floor division of a and b:
+print('floor divion of a and b is :',a//b)
+
+#a power b
+print('a power b:',a**b)
+
+#Find out whether two variables have equal value or not - if yes then print True if they are not equal then print False
+if(a==b):
+    print('true')
+else:
+    print('false')
