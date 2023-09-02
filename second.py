101da85b13edb12cc0f03e2e85b267081f785dcd
Arpitasenapati <108747344+Arpitasenapati@users.noreply.github.com>
2023-09-02 19:36:24 +0530

    Add files via upload

---
+++ second.py
@@ -0,0 +1,5 @@
+#Write a program that takes side of a square as input and calculates the area of this square by using area() function.
+side=int(input('enter the side of the square:'))
+def area():
+    print(side*side)
+area()
